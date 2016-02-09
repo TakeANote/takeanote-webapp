aced83b9b547a41933b10b03bae5c6cdf916a2e0
Thomas Chafiol <thomaschaf@gmail.com>
2016-02-09 11:14:53 -0800

    Improve auth

---
+++ app/scripts/auth.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+
+    var app = document.querySelector("#app");
+
+    app.login = function(key) {
+        Cookies.set('api-key', key);
+    }
+
+    app.isLogged = function() {
+        return (Cookies.get('api-key') !== undefined);
+    }
+
+    app.logout = function() {
+        Cookies.remove('api-key');
+    }
+})();
